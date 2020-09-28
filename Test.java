d00bc04cbfa9f874b9e524eae204afed21a67cd4
javanotes <lakshmikoteswari36462@gmail.com>
2020-09-28 14:28:40 +0530

    Added 3 files

---
+++ Test.java
@@ -0,0 +1,12 @@
+class Testcode
+{
+  public static void main(String a[])
+{
+   System.out.println("Welcome to project of CI/CD");
+   System.out.println("Added another line of code");
+   System.out.println("Added another line again");
+   System.out.println("Added another line again11111"); 
+   System.out.println("Added another line again2222");     
+
+}
+}
---
+++ Testcode.java
@@ -0,0 +1,8 @@
+class Testcode
+{
+  public static void main(String a[])
+{
+   System.out.println("Welcome to project of CI/CD");
+   System.out.println("Added another line of code");
+}
+}
